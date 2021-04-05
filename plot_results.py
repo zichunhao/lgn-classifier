4c2bacca1a0c1306753deb2b5bf8726eb1dccbba
Zichun Hao <zichunh2000@gmail.com>
2021-04-05 09:45:26 -0700

    Implement ROC curves

--- plot_results.py
+++ plot_results.py
@@ -11,9 +11,9 @@ def plot_confusion_matrix(args, confusion_matrix, epoch, outpath, is_train):
     ax.set_title(f'Confusion matrix at epoch {epoch+1}')
     ax.set_xlabel('Predicted labels')
     ax.set_ylabel('True labels')
-    ax.invert_yaxis()
-    ax.xaxis.set_ticklabels(['g', 'q', 't', 'w', 'z'])
-    ax.yaxis.set_ticklabels(['g', 'q', 't', 'w', 'z'])
+    ax.invert_yaxis()  # y axis increasing upward
+    ax.xaxis.set_ticklabels(args.class_labels)
+    ax.yaxis.set_ticklabels(args.class_labels)
     if is_train:
         plt.savefig(f'{outpath}/confusion_matrix_train_epoch_{epoch+1}.{args.fig_format}')
     else:
